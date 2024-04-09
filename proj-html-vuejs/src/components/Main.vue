54910aaabe9e51cb85f749a91a881ec44197649f
EugenioCaiazzo <ecaiazzo22@fed.idserve.net>
2024-04-09 12:06:29 +0200

    1 jumbotron

--- proj-html-vuejs/src/components/Main.vue
+++ proj-html-vuejs/src/components/Main.vue
@@ -9,8 +9,26 @@ export default {
 
 <template>
 
+<!--1 Jumbotron-->
+<div class="container d-flex" id="1Jumbotron">
+    <div style="padding-left: 15%; width: 50%;" class="align-content-center"> 
+        <h1> Udemy Affiliate Sales</h1>
+        <h5> Monetize your audience and attract new customers with Udemy!</h5>
+    </div>
+    <div style="width: 50%;">
+        <img class="float-end" src="./images/naassom-azevedo-Q_Sei-TqSlc-unsplash.jpg" alt="Foto di Naassom Azevedo su Unsplash" style="width: 100%; height: 400px; object-fit: cover;">
+    </div>
+</div>
+
 </template>
 
 <style lang="scss" scoped>
 
+#1Jumbotron{
+  background-color: #ff4550; 
+  color: white; 
+  height: 400px;
+  text-align: center;
+  line-height: 100%;
+}
 </style>
