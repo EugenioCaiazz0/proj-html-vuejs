02ea499a2c0a897254ea5f20a0d2790c552b49ed
EugenioCaiazzo <ecaiazzo22@fed.idserve.net>
2024-04-10 10:05:54 +0200

    Footer e fine scheletro

---
+++ proj-html-vuejs/src/components/Footer.vue
@@ -0,0 +1,61 @@
+<script>
+
+</script>
+
+<template>
+<footer>
+   
+    <div class="container d-flex flex-row" style="padding-top: 60px;">
+        <div class="footerElement">
+            <h5>ABOUT</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos minus saepe aspernatur, porro reiciendis expedita aliquam, incidunt nemo, tempore sit perspiciatis quae alias pariatur nostrum! Autem quasi delectus incidunt voluptatum!</p>
+        </div>
+        <div class="footerElement">
+            <h5>CONTACT</h5>
+            <p>USA, California 20, First Avenue, California</p>
+            <p>Tel: + lorem ipsum</p>
+            <p>Tel: + lorem ipsum</p>
+        </div>
+        <div class="footerElement">
+            <h5>SOCIAL NETWORK</h5>
+            <div class="d-flex flex-wrap">
+                <!--icons-->
+            </div>
+        </div>
+        <div class="footerElement">
+            <img class="footerBanner" src="images/Top-udemy-courses-90-percent-off.jpg" alt="">
+        </div>
+    </div>
+    <hr>
+    <div class="container d-flex flex-row">
+        <img src="images/footer-logo2x-50x56.png" class="logo" alt="">
+        <p>Copyright Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi blanditiis mollitia iure aliquam officia omnis!</p>
+    </div>
+
+</footer>
+</template>
+
+<style>
+
+#footer{
+    height: 370px; 
+    background-color: #273044; 
+    color: white;
+}
+
+.logo{
+    width: 27px; 
+    height: 27px; 
+    margin-right: 10px;
+}
+
+.footerElement{
+    width: 300px;
+}
+
+.footerBanner{
+    width: 270px; 
+    height: 170px;
+}
+
+</style>
